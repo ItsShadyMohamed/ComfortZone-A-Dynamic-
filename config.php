87a20e4142d2eea370ceb00a37781e2e192c2a6a
Shady Mohamed <111459312+SheedoM@users.noreply.github.com>
2023-05-12 19:41:23 +0300

    config.php

--- config.php
+++ config.php
@@ -1,5 +1,9 @@
 <?php
-$conn= mysqli_connect('localhost','root','','comfortzone_database');
+$host = "localhost";
+$username = "your_username";
+$password = "your_password";
+$database = "your_database";
+$conn= mysqli_connect($host,$username,$password,$database);
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
   }
